0fca4a0994eb56757336658bd1889887c97d1f79
AndrewRamyE <81973091+AndrewRamyE@users.noreply.github.com>
2022-06-22 03:27:09 +0200

    init5

--- frontend/src/components/authPage/AuthPage.js
+++ frontend/src/components/authPage/AuthPage.js
@@ -28,7 +28,8 @@ function AuthPage() {
     useEffect(()=>{
         console.log(token);
         let check = async ()=>{
-            await axios.get(`${process.env.REACT_APP_BASE_URL}/user/checkauth`,{headers})
+            // await axios.get(`${process.env.REACT_APP_BASE_URL}/user/checkauth`,{headers})
+            await axios.get(`/user/checkauth`,{headers})
                 .then(function ({data}) {
                     console.log(data);
                     navigate('/task');
@@ -46,7 +47,8 @@ function AuthPage() {
           'email':signinEmail,
           'password':signinPassword
         }
-        await axios.post(`${process.env.REACT_APP_BASE_URL}/user/signin`,data,{headers})
+        // await axios.post(`${process.env.REACT_APP_BASE_URL}/user/signin`,data,{headers})
+        await axios.post(`/user/signin`,data,{headers})
         .then(function ({data}) {
             if (data.status === 200) {
                 data = JSON.parse(data.data);
@@ -66,7 +68,8 @@ function AuthPage() {
             'email':loginEmail,
           'password':loginPassword
         }
-          await axios.post(`${process.env.REACT_APP_BASE_URL}/user/login`,data,{headers})
+        //   await axios.post(`${process.env.REACT_APP_BASE_URL}/user/login`,data,{headers})
+          await axios.post(`/user/login`,data,{headers})
           .then(function ({data}) {
             if (data.status === 200) {
                 data = JSON.parse(data.data);
